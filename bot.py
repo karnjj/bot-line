0709becea6bd8f354ed533450e2ace674dea4c31
karnjj <karnjj@hotmail.com>
2018-12-04 20:29:00 +0700

    make it better

--- bot.py
+++ bot.py
@@ -8,6 +8,12 @@ def hello():
 
 	return "Hello World!"
 
+@app.route("/webhook", methods=['GET', 'POST'])
+
+def webhook():
+	if request.method == 'POST':
+		return 'OK'
+
 if __name__ == "__main__":
 
 	app.run()
