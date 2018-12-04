04d41d3ee87b88ed3abec0396264657a29c8f497
karnjj <karnjj@hotmail.com>
2018-12-05 01:00:20 +0700

    make it better

--- bot.py
+++ bot.py
@@ -2,6 +2,41 @@ from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
+import paho.mqtt.client as mqtt
+import os, urlparse
+
+# Define event callbacks
+def on_connect(client, userdata, flags, rc):
+    print("rc: " + str(rc))
+
+def on_message(client, obj, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+def on_publish(client, obj, mid):
+    print("mid: " + str(mid))
+
+def on_subscribe(client, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+def on_log(client, obj, level, string):
+    print(string)
+
+mqttc = mqtt.Client()
+# Assign event callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+
+url_str = os.environ.get('mqtt://brsiutlc:Rw4rcSFm_gCL@m15.cloudmqtt.com:17711/test')
+url = urlparse.urlparse(url_str)
+topic = url.path[1:] or 'test'
+
+mqttc.username_pw_set(url.username, url.password)
+mqttc.connect(url.hostname, url.port)
+
+mqttc.subscribe(topic, 0)
+
 
 app = Flask(__name__)
 
@@ -31,10 +66,10 @@ def webhook():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 	text=event.message.text
-	if text == 'karn' :		
-		line_bot_api.reply_message(
-			event.reply_token,
-			TextSendMessage('Hello karn!!'))
+	line_bot_api.reply_message(
+		event.reply_token,
+		TextSendMessage(text))
+	mqttc.publish(topic, text)
 
 if __name__ == "__main__":
 	app.run()
