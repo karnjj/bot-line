bfcd7027b005500b64e493a7f666be3e72cd442c
karnjj <karnjj@hotmail.com>
2018-12-04 20:34:33 +0700

    make it better

--- bot.py
+++ bot.py
@@ -3,16 +3,15 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/webhook", methods=['GET', 'POST'])
 
 def hello():
 
 	return "Hello World!"
 
-@app.route("/webhook", methods=['GET', 'POST'])
-
 def webhook():
 	if request.method == 'POST':
-		return 'OK'
+	return 'OK'
 
 if __name__ == "__main__":
 
