e21bd77c3688323a01158603508e5934a9a967ee
karnjj <karnjj@hotmail.com>
2018-12-05 03:25:22 +0700

    make it better

--- bot.py
+++ bot.py
@@ -68,6 +68,9 @@ def handle_message(event):
 		event.reply_token,
 		TextSendMessage(text))
 	mqttc.publish("/test1", text)
+	line_bot_api.reply_message(
+		event.reply_token,
+		TextSendMessage(str(msg.payload)))
 
 if __name__ == "__main__":
 	app.run()
