dd555f4ed69903882918f5158d3c4bcb89d1c63e
karnjj <karnjj@hotmail.com>
2018-12-05 16:45:44 +0700

    make it better

--- bot.py
+++ bot.py
@@ -15,10 +15,10 @@ def on_message(client, obj, msg):
     global temp
     m_in=json.loads(msg.payload)
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-    txt = str(m_in["temp"]) + " " + str(m_in["humi"]) + " " + str(bool(m_in["mois"]))
+    print(m_in["temp"])
     line_bot_api.reply_message(
 		temp.reply_token,
-		TextSendMessage(txt))
+		TextSendMessage("TEMP : {0}\nHUMI : {1}\nSOIL : {2}\nLUMI : {3}" .format(str(m_in["temp"]),str(m_int["humi"]),str(bool(m_in["mois"])),str(m_in["lumi"]))))
     mqttc.disconnect()
 
 def on_publish(client, obj, mid):
