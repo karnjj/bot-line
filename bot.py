2730bc8522785f961802658f314c54622d6b47d0
Karnjj <karnjj@hotmail.com>
2019-05-05 15:55:52 +0700

    a

--- bot.py
+++ bot.py
@@ -181,7 +181,7 @@ def callback():
 def handle_message(event):
     global temp, loop_flag
     count = 0
-    print(type(event.source))
+    print(event.source.user_id)
     cfg.read('config.ini')
     temp = event
     mqttc.username_pw_set("brsiutlc", "Rw4rcSFm_gCL")
